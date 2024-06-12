3a85ee0bf9c286e67abad0bcc91b60e7c9980c07
AlexNorman11 <168736422+AlexNorman11@users.noreply.github.com>
2024-06-12 14:31:33 +0100

    deploy fourth

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 export { default as NavBar } from './NavBar/NavBar';
 export { default as ScrollTop} from './ScrollTop';
+export { default as AnIcon} from './logo';
 
--- src/Containers/index.jsx
+++ src/Containers/index.jsx
@@ -1,10 +1,10 @@
 export { default as Home } from './Home/Home';
 export { default as Resume } from './Resume/Resume';
-export { default as About } from './About/About'
-export { default as Demo } from './Demo/Demo'
-export { default as Contact } from './Contact/Contact'
-export { default as Footer } from './Footer/Footer'
-export { default as Projects } from './Projects/Projects'
+export { default as About } from './About/About';
+export { default as Demo } from './Demo/Demo';
+export { default as Contact } from './Contact/Contact';
+export { default as Footer } from './Footer/Footer';
+export { default as Projects } from './Projects/Projects';
 
 
 
